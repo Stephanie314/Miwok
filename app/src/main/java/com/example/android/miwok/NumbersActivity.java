af6d0eea4d4b52be657e12ae0570fe41bc76f212
Stephanie314 <stephanieanderson314@gmail.com>
2018-07-05 20:10:59 -0700

    While loop conditions - Lesson 3.11

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,10 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +28,39 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Create an array of words
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        //Create the root view in order to add child views to it
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+        //Create a variable to keep track of the current index position
+        //Make a loop to make TextViews as long as the current index is less than the length of the list
+
+        int index = 0;
+        while(index < words.size()){
+
+            //Create a new TextView
+            //Set the text to be the word at the current index
+            //Add this TextView as a child of the rootView of the layout
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+
+            //Increment counter variable
+            index++; //index = index + 1;
+        }
+
     }
 }
